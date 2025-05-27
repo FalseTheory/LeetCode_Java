c3c9615f647aebe16116f694005cbb194023b523
Fairgraves <rozaco0@gmail.com>
2025-05-27 21:42:50 +0700

    May 27 2025 daily

---
+++ Easy/src/DivisibleAndNonDivisibleSumsDifference/Solution.java
@@ -0,0 +1,15 @@
+package DivisibleAndNonDivisibleSumsDifference;
+
+public class Solution {
+    public int differenceOfSums(int n, int m) {
+        int num1 = 0;
+        int sum = n * (n + 1) / 2;
+
+        for(int i = 0; i <= n; i++) {
+            if(i % m != 0) {
+                num1+=i;
+            }
+        }
+        return num1 - (sum - num1);
+    }
+}
